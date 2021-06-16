cb79bc8ee99a6e8d7607af40b8242dd47f320f6a
Djigui <djigui@1F-01.edu-il.it>
2021-06-16 15:34:21 +0200

    terminato terzo esercizio

--- 03-celsius_fahrenheit/celsius_fahrenheit.js
+++ 03-celsius_fahrenheit/celsius_fahrenheit.js
@@ -1,12 +1,20 @@
 // define a function multiplyByNineFifths(number)
-
+const multiplyByNineFifths = (X) => {
     //return number*(9/5)
-
-// define a function getFahrenheit(celsius)
-
+    return X * (9 / 5)
+    // define a function getFahrenheit(celsius)
+}
+const getFahrenheit = (p) => {
     // return multiplyByNineFifths(celsius) + 32;
-
-console.log() // call getFahrenheit();
-
-
+    return multiplyByNineFifths(p) + 32;
+}
+console.log(getFahrenheit(40)); // call getFahrenheit();
 // do the same thing starting from Fahrenheit to Celsius
+//`°C = (°F − 32) × 5/9`
+const celsiuConverssione = y => {
+    return y-32;
+}
+const celsiUscon = risultato => {
+    return celsiuConverssione(risultato) * 5/9 ;
+}
+console.log(celsiUscon(273));
