891069514ca42f4a8140b648c9d4140040e8c909
Djigui Tounkara <tounkaradjg@gmail.com>
2021-07-09 23:01:47 +0200

    esercitazione condizionali e increments the numbers

--- 01-can_you_vote/can_you_vote.js
+++ 01-can_you_vote/can_you_vote.js
@@ -13,3 +13,72 @@ const legalAge = 18;
         console.log("******");
     
         canIVote(12);
+
+
+let elezione = Age => {
+    let bestAge = 18
+    if (Age > bestAge) {
+        console.log("can eat a apple");
+
+    } if ( Age >= bestAge) {
+        console.log( " can eat a apple and a mango ");
+
+    } else {
+        console.log( " can't eat apple not mango");
+
+    }
+}
+elezione(18)
+console.log("     ")
+elezione(22);
+
+const viaggiare = andare => {
+    let viaggio = napoli
+    if (viaggio = andare) {
+        console.log("andiamo a napoli ")
+    }
+    else {
+        console.log( "non si va a napoli");
+
+    }
+
+}
+  
+
+const mamma = papa => {
+    let salif = 19;
+if ( salif >= papa) {
+console.log("allora salif e troppo grande ");
+
+}else  {
+    console.log( "altrimenti papa e molto grande di salif");
+
+}
+
+}
+mamma(20);
+
+
+
+let me =19
+let medam = 29;
+if (me >= medam) {
+    console.log("allora adama deve pagare ");
+
+} else {
+    console.log("allora dovro pagare io perche ho detto una cosa sbagliato ");
+
+}
+
+let x = 20;
+let z = 201;
+if ( x >= z ) {
+    console.log( "allora x è un numero più fortunato in ");
+   
+    } else if( z > x  ) {
+        console.log("allora z è davvero quello che bisognava  ");
+    } 
+let g1 = 200;
+for(let i = g1; i--;) { 
+console.log(i);
+}
